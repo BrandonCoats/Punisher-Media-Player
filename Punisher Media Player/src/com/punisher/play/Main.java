6c4b14fdf6f6a2ec6e3220ff7a271cbb23fb3f88
BrandonCoats <bgcoats1313@gmail.com>
2016-11-21 08:45:10 -0700

    Seperatred into class

--- Punisher Media Player/src/com/punisher/play/Main.java
+++ Punisher Media Player/src/com/punisher/play/Main.java
@@ -1,23 +1,9 @@
 package com.punisher.play;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Main  extends Application{
+public class Main {
 
 	public static void main(String[] args) {
-		launch();
+		Player player = new Player();
+		player.Launcher();
 	}
-public void start(Stage primaryStage) throws Exception
-{
-	Button button = new Button("Play Music");
-	StackPane root = new StackPane();
-	root.getChildren().add(button);
-	Scene scene = new Scene(root);
-	primaryStage.setScene(scene);
-	primaryStage.show();
-}
 }
