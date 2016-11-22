1a55afb1a9c91950c69a7c2e8713d068ced35df9
JakoAlltrades <Jpriemo@gmail.com>
2016-11-22 08:13:55 -0700

    Music File now plays
    Had to create a JFXPanel to use JFX features

--- Punisher Media Player/src/com/punisher/play/Main.java
+++ Punisher Media Player/src/com/punisher/play/Main.java
@@ -1,13 +1,17 @@
 package com.punisher.play;
 
+import javafx.embed.swing.JFXPanel;
+
 public class Main {
 
 	public static void main(String[] args) {
+		new JFXPanel();
 		//Player player = new Player();
 		//player.Launcher();
 		
 		//throwing error
-		//AudioFile audio = new AudioFile();
-		//audio.PlayAudioFile();
+		
+		AudioFile audio = new AudioFile();
+		audio.PlayAudioFile();
 	}
 }
